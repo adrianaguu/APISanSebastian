7c8b42ab0dd67651734cf0dcc56e87da55b2c032
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2020-06-17 02:21:38 -0500

    update

--- products/urls.py
+++ products/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('brands/', BrandList.as_view()),
     path('products/', ProductList.as_view()),
     path('lines/', LineList.as_view()),
+    path('products/filter_category/<int:pk>', ProductByCategory.as_view()),
+    path('products/filter_brand/<int:pk>', ProductByBrand.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- products/views.py
+++ products/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 from .models import *
 from .serializers import *
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import HttpResponse, JsonResponse
+
 
 
 class CategoryList(generics.ListCreateAPIView):
@@ -20,3 +25,33 @@ class LineList(generics.ListCreateAPIView):
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class ProductByCategory(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, pk, format=None):
+    	try:
+    		category = Category.objects.get(pk=pk)
+    	except Category.DoesNotExist:
+    		return HttpResponse(status=404)
+
+    	products = Product.objects.filter(category=category)
+    	serializer = ProductSerializer(products, many=True)
+    	return Response(serializer.data)
+
+class ProductByBrand(APIView):
+    """
+    List all snippets, or create a new snippet.
+    """
+    def get(self, request, pk, format=None):
+    	try:
+    		brand = Brand.objects.get(pk=pk)
+    	except Brand.DoesNotExist:
+    		return HttpResponse(status=404)
+
+    	products = Product.objects.filter(brand=brand)
+    	serializer = ProductSerializer(products, many=True)
+    	return Response(serializer.data)
+
